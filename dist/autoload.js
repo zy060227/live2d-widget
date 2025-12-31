df16ba79a1c8957bd6fe3fbbcd6e173761e84df1
zy060227 <246769228@qq.com>
2025-12-31 09:43:12 +0800

    Update autoload.js

--- dist/autoload.js
+++ dist/autoload.js
@@ -5,7 +5,7 @@
 
 // Recommended to use absolute path for live2d_path parameter
 // live2d_path 参数建议使用绝对路径
-const live2d_path = 'https://fastly.jsdelivr.net/gh/zy060227/live2d-widget@1.0.1/dist/';
+const live2d_path = 'https://fastly.jsdelivr.net/gh/zy060227/live2d-widget@1.0.0/dist/';
 // const live2d_path = '/dist/';
 
 // Method to encapsulate asynchronous resource loading
